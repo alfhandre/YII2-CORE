21606ee5efa542cebbe64fa1490b3a94bc4178fa
Muhammad Fadhil Maulana <95394393+ucupofcoffee@users.noreply.github.com>
2022-03-13 21:25:34 +0700

    Update modul.c

--- modul.c
+++ modul.c
@@ -1,98 +1,84 @@
 #include <stdio.h>
-#include "header.h"
 #include <windows.h>
-#include <conio.h>
-#include <unistd.h>
+#include "header.h"
 
-void tampilan(){
-	format (9);
-	printf("==========================================\n");
-	printf("       text editor Yii2 Core (BETA)       \n");
-	printf("==========================================\n");
+int main(){
 	
-}
+int menu;
 
-void panduan(){
-	format (6);
-	printf("==========================================\n");
-	printf("       			PANDUAN				      \n");
-	printf("==========================================\n");
-	printf("1. Pilih menu 1 untuk memulai text editor\n");
-	printf("2. Untuk keluar tekan enter 3 kali");
-	printf("\n\n\nTekan Enter Untuk Kembali Ke Menu\n");
-	printf("==========================================\n");
-}
-
-void bukafile(){
-	char buff[255];
-	FILE *fptr;
-	char namafile[30];
-	
-	//nama file yang akan di buka  
-	format (3);
-	printf("=============================================================\n");
-	printf(" Nama File yang akan di buka :");
-	gets(namafile);
-	system("cls");
-	
-	
-	//operasi file 
-	if((fptr = fopen(namafile,"r")) == NULL){
-		printf("ERORR! Data tidak ditemukan\n");
-		printf("\nTekan Enter Untuk Kembali");
-	}
-	while(fgets(buff, sizeof(buff),fptr)){
-		printf("%s", buff);
-	}
-
-	fclose(fptr);
-}
-
-void buatfile(){
-	char buff[255];
-	FILE *fptr;
-	char namafile[30];
-	char c;
-	
-	//nama file yang akan di buat  
-	
-	printf("\nNama File yang akan dibuat :");
-	gets(namafile);
-	system("cls");
-	tampilan();
-	
-	//operasi file 
-		fptr = fopen(namafile,"w+"); 
+	while(1){
 		
-        // mengambil input dari user
-		while(1)
-		{
-		c=getchar();
+		menu = 0;
+		printf("==========================================\n");
+		printf("       Text Editor Yii2 Core (BETA)       \n");
+		printf("==========================================\n");
+		printf("1. Membuat Text   \n");
+		printf("2. Buka File      \n");
+		printf("3. Panduan        \n");
+		printf("4. Duplikasi File \n");
+		printf("5. Ganti Nama File\n");
+		printf("6. keluar         \n");
+		printf("\nPilih Menu : ");
+		scanf("%d",&menu);fflush(stdin);
 		
-       		 if(c==32)
-		{ 
-		c =' ';
-		fputc(c, fptr);
-		} 
-		else if(c == 19)
-		{
-			system("cls");
-			tampilan();
-			printf("\n\tFile Berhasil Di Save");
-			printf("\n\n\tTekan Enter untuk keluar");
-			fclose(fptr);
-			break;
+		switch(menu){
+			case 1 :
+			{
+				system("cls");
+				newFile();
+//				textEditor();
+				getch();
+				system("cls");
+				break;
+			}
+				case 2 :
+			{
+				system("cls");
+				openFile();
+				getch();
+				system("cls");
+				break;
+			}
+			case 3 :
+			{
+				system("cls");
+				panduan();
+				getch();
+				system("cls");
+				break;
+			}
+			case 4 :
+			{
+				system("cls");
+				duplicateFile();
+				getch();
+				system("cls");
+				break;
+			}
+			case 5 :
+			{
+				system("cls");
+				renameFile();
+				getch();
+				system("cls");
+				break;
+			}
+			case 6 :
+			{
+				return(0); 
+				break;
+			}
+				default:
+			{
+				system("cls");
+				printf("\t=======================\n");
+				printf("\t     Menu tidak ada    \n");
+				printf("\t=======================\n");
+				sleep(2);
+				system("cls");
+			
+				break;
+			}
 		}
-		else{
-		fputc(c, fptr);
-		}
-	
-	}
-    
+	}	
 }
-
-void format (int i){
-	HANDLE j = GetStdHandle (STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute (j,i);
-}
-
