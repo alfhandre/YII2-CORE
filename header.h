4a9b2cba7414bf443f2b95259534618312e9657b
salmaaush <100129212+salmaaush@users.noreply.github.com>
2022-03-16 19:43:22 +0700

    update header

---
+++ header.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include <unistd.h>
+#ifndef header_h
+#define header_h
+#define MAX 100
+#define LEN 80
+
+
+
+void texteditor();
+void tampilan();
+void panduan();
+void bukafile();
+void buatfile();
+void format();
+void duplicateFile();
+void renameFile();
+void jum_kata();
+void jum_char();
+
+#endif
