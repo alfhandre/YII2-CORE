b614d424ed399bcfdf9319cdb24ee43df26e2d34
Muhammad Fadhil Maulana <95394393+ucupofcoffee@users.noreply.github.com>
2022-03-24 14:52:56 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,80 +2,9 @@
 #include <stdlib.h>
 #include "header.h"
 
-int main(void){
-	int menu;
-
-	while(1){
-		
-		tampilan();
-		menu = 0;
-		format (11);
-		printf("1. Membuat Text   \n");
-		printf("2. Buka File      \n");
-		printf("3. Panduan        \n");
-		printf("4. keluar         \n");
-		format (10);
-		printf("\nPilih Menu :");
-		scanf("%d",&menu);fflush(stdin);
-		
-		switch(menu){
-			case 1 :
-			{
-				system("cls");
-				buatfile();
-				getch();
-				system("cls");
-				break;
-			}
-				case 2 :
-			{
-				system("cls");
-				bukafile();
-				getch();
-				system("cls");
-				break;
-			}
-			
-			case 3 :
-			{
-				system("cls");
-				panduan();
-				getch();
-				system("cls");
-				break;
-			}
-			case 4 :
-			{
-				system("cls");
-				duplicateFile();
-				getch();
-				system("cls");
-				break;
-			}
-			case 5 :
-			{
-				system("cls");
-				renameFile();
-				getch();
-				system("cls");
-				break;
-			}
-			case 6 :
-			{
-				return(0); 
-				break;
-			}
-				default:
-			{
-				system("cls");
-				printf("\t=======================\n");
-				printf("\t     Menu tidak ada    \n");
-				printf("\t=======================\n");
-				sleep(2);
-				system("cls");
-			
-				break;
-			}
-		}
-	}	
+int main()
+{
+  
+  menu();
+  
 }
